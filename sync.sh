55e1148f9bc4c2f672556f31507ff38f571ecef8
khirendra03 <khirendrapatel3@gmail.com>
2022-12-05 17:45:07 +0530

    Theme: hellfire
    Signed-off-by: khirendra03 <khirendrapatel3@gmail.com>

--- sync.sh
+++ sync.sh
@@ -19,22 +19,26 @@ git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
 
 # clang 14.0.1
-git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
+#git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
+git clone --depth=1 https://github.com/nerdprojectorg/SDClang.git -b 14 prebuilts/clang/host/linux-x86/clang-sdllvm
 git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 14 prebuilts/clang/host/linux-x86/trb_clang
 
 rm -rf hardware/qcom-caf/msm8998/display
 #git clone https://github.com/VoltageOS-staging/hardware_qcom_display -b 13-msm8998 hardware/qcom-caf/msm8998/display
-git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_display -b thirteen hardware/qcom-caf/sdm660/display
+#git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_display -b thirteen hardware/qcom-caf/sdm660/display
+git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_display.git -b tm android_hardware_qcom-caf_sdm660_display
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/audio
+#git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/audio
 #git clone https://github.com/VoltageOS-staging/hardware_qcom_audio -b 13-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_audio.git -b tm android_hardware_qcom-caf_sdm660_audio
 
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/media
+#git clone https://github.com/khirendra03/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/media
+git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_media.giy -b tm android_hardware_qcom-caf_sdm660_media
 
-rm -rf vendor/qcom/opensource/commonsys-intf/display
-git clone https://github.com/NerdZ3ns/android_vendor_qcom_opensource_display-commonsys-intf.git -b arrow-13.0 vendor/qcom/opensource/commonsys-intf/display
+#rm -rf vendor/qcom/opensource/commonsys-intf/display
+#git clone https://github.com/NerdZ3ns/android_vendor_qcom_opensource_display-commonsys-intf.git -b arrow-13.0 vendor/qcom/opensource/commonsys-intf/display
 #git clone https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys-intf.git -b lineage-20.0 vendor/qcom/opensource/commonsys-intf/display
 
 # Exit
