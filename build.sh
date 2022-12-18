14a9c34ca6ecba966cbe359fe500c7cb70ad7f6d
khirendra <khirendrapatel3@gmail.com>
2022-12-18 11:38:55 +0530

    Font: adding fonts from powerline fonts
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ fi
 # Run the Extra Command
 #$EXTRA_CMD
 
-cp -af hardware/qcom-caf/common/os_pickup.bp hardware/qcom-caf/sdm660/Android.bp && cp -af hardware/qcom-caf/common/os_pickup.mk hardware/qcom-caf/sdm660/Android.mk
+#cp -af hardware/qcom-caf/common/os_pickup.bp hardware/qcom-caf/sdm660/Android.bp && cp -af hardware/qcom-caf/common/os_pickup.mk hardware/qcom-caf/sdm660/Android.mk
 
 # Prepare the Build Environment
 source build/envsetup.sh
--- sync.sh
+++ sync.sh
@@ -19,30 +19,30 @@ git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
 
 # clang 14.0.1
-#git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
-#git clone --depth=1 https://github.com/nerdprojectorg/SDClang.git -b 14 prebuilts/clang/host/linux-x86/clang-sdllvm
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
+git clone --depth=1 https://github.com/nerdprojectorg/SDClang.git -b 14 prebuilts/clang/host/linux-x86/clang-sdllvm
 git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 14 prebuilts/clang/host/linux-x86/trb_clang
 
 #rm -rf hardware/qcom-caf/msm8998/display
 #git clone https://github.com/VoltageOS-staging/hardware_qcom_display -b 13-msm8998 hardware/qcom-caf/msm8998/display
 #git clone https://github.com/Asus-Max-Pro-M2/hardware_qcom-caf_sdm660_display -b thirteen hardware/qcom-caf/sdm660/display
-#git clone https://github.com/khirendra03/android_hardware_qcom-caf_sdm660_display.git -b tm hardware/qcom-caf/sdm660/display
-git clone https://github.com/SDM660-lavender/android_hardware_qcom_display.git -b 13-caf_4.19 hardware/qcom-caf/sdm660/display
+git clone https://github.com/khirendra03/android_hardware_qcom-caf_sdm660_display.git -b tm hardware/qcom-caf/sdm660/display
+#git clone https://github.com/SDM660-lavender/android_hardware_qcom_display.git -b 13-caf_4.19 hardware/qcom-caf/sdm660/display
 
 #rm -rf hardware/qcom-caf/msm8998/audio
 #git clone https://github.com/Asus-Max-Pro-M2/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/audio
 #git clone https://github.com/VoltageOS-staging/hardware_qcom_audio -b 13-msm8998 hardware/qcom-caf/msm8998/audio
-#git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_audio.git -b tm hardware/qcom-caf/sdm660/audio
-git clone https://github.com/SDM660-lavender/android_hardware_qcom_audio.git -b 13-caf-sdm660 hardware/qcom-caf/sdm660/audio
+git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_audio.git -b tm hardware/qcom-caf/sdm660/audio
+#git clone https://github.com/SDM660-lavender/android_hardware_qcom_audio.git -b 13-caf-sdm660 hardware/qcom-caf/sdm660/audio
 
 #rm -rf hardware/qcom-caf/msm8998/media
 #git clone https://github.com/Asus-Max-Pro-M2/hardware_qcom-caf_sdm660_media -b thirteen hardware/qcom-caf/sdm660/media
-git clone https://github.com/SDM660-lavender/android_hardware_qcom_media.git -b 13-caf-sdm660 hardware/qcom-caf/sdm660/media
-#git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_media.git -b tm hardware/qcom-caf/sdm660/media
+#git clone https://github.com/SDM660-lavender/android_hardware_qcom_media.git -b 13-caf-sdm660 hardware/qcom-caf/sdm660/media
+git clone https://github.com/nerdprojectorg/android_hardware_qcom-caf_sdm660_media.git -b tm hardware/qcom-caf/sdm660/media
 
 #rm -rf vendor/qcom/opensource/commonsys-intf/display
 #git clone https://github.com/NerdZ3ns/android_vendor_qcom_opensource_display-commonsys-intf.git -b arrow-13.0 vendor/qcom/opensource/commonsys-intf/display
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys-intf.git -b lineage-20.0 vendor/qcom/opensource/commonsys-intf/display
+#git clone https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys-intf.git -b lineage-20.0 vendor/qcom/opensource/commonsys-intf/display
 
 # Exit
 exit 0
